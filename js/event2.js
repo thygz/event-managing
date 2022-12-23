9bf8eb5c98990229cee7c45e964d6351ee479adc
Teejay Villaluz <traxxjester25@gmail.com>
2022-12-24 00:36:47 +0800

    initial commit

---
+++ js/event2.js
@@ -0,0 +1,26 @@
+// var swiper = new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor: true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
